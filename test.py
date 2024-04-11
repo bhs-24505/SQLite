125bc50bd20f4c6fce7820ca35e6de9181824565
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-11 12:33:42 +1200

    first

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import sqlite3
+
 DATABASE = 'cars.db'
-with sqlite3.connect(DATABASE) as db:
-  cursor = db.cursor()
-  sql = 'SELECT * FROM car;'
-  cursor.execute(sql)
-  results = cursor.fetchall()
-  # print them nicely
-  for car in results:
-    print(f'Car: {car[1]} Top Speed: {car[2]}')
+
+def print_all_cars():
+  speed = input('What speed: ')
+  with sqlite3.connect(DATABASE) as db:
+    cursor = db.cursor()
+    sql = 'SELECT car_name,top_speed FROM car WHERE top_speed > ?;'
+    cursor.execute(sql,(speed,))
+    results = cursor.fetchall()
+    # print them nicely
+    for car in results:
+      print(f'Car: {car[0]} Top Speed: {car[1]}')
+
+if __name__ == '__main__':
+    print_all_cars()
