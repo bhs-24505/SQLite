2c5d034eef640e925909d9faeda99cad27f26b3e
bhs-24505 <166450492+bhs-24505@users.noreply.github.com>
2024-06-05 13:55:37 +1200

    Add more code comments

--- airplanes.py
+++ airplanes.py
@@ -5,10 +5,12 @@ DATABASE = "airplanes.db"
 
 #functions
 
+#function 1
 def print_all_airplanes():
     '''print all the airplanes'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print all airplanes information 
     sql = 'SELECT * FROM airplanes'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -18,10 +20,12 @@ def print_all_airplanes():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 2
 def print_all_airplanes_by_wingspan():
     '''print all the airplanes sorted by wingspan'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their wingspans from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY wingspan DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -31,10 +35,12 @@ def print_all_airplanes_by_wingspan():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 3
 def print_all_airplanes_by_length():
     '''print all the airplanes sorted by length'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their lengths from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY length DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -44,10 +50,12 @@ def print_all_airplanes_by_length():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 4
 def print_all_airplanes_by_max_speed():
     '''print all the airplanes sorted by max speed'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their max spped from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY max_speed DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -57,10 +65,12 @@ def print_all_airplanes_by_max_speed():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 5
 def print_all_airplanes_by_max_range():
     '''print all the airplanes sorted by max range'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their max range from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY max_range DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -70,10 +80,12 @@ def print_all_airplanes_by_max_range():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 6
 def print_all_airplanes_by_max_passenger():
     '''print all the airplanes sorted by max passenger'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their max passengers from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY max_passenger DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -83,10 +95,12 @@ def print_all_airplanes_by_max_passenger():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 7
 def print_all_airplanes_by_max_fuel():
     '''print all the airplanes sorted by max fuel'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print airplanes by their max fuel from highest to lowest
     sql = 'SELECT * FROM airplanes ORDER BY max_fuel DESC'
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -96,6 +110,7 @@ def print_all_airplanes_by_max_fuel():
         print(f'{airplane[1]:<27}{airplane[2]:<10}{airplane[3]:<8}{airplane[4]:<11}{airplane[5]:<11}{airplane[6]:<15}{airplane[7]:<10}{airplane[8]:<8}')
     db.close()
 
+#function 8
 def search_and_print_airplane():
     '''search and print airplanes by id'''
     db = sqlite3.connect(DATABASE)
@@ -104,13 +119,17 @@ def search_and_print_airplane():
     cursor.execute(sql)
     results = cursor.fetchall()
     print(f'id    model')
+    #print out the ids and models for the user to choose
     for model in results:
         print(f'{model[0]:<6}{model[1]:<12}')
     print()
+    # a user input 
     id = int(input('Enter id to view details:\n'))
     sql = 'SELECT * FROM airplanes WHERE id=?'
     cursor.execute(sql, (id,))
+    # only fetch the one which the user choose
     airplane = cursor.fetchone()
+    #print out all inforamtion about that airplane
     if airplane:
         print(f'Airplane information:')
         print(f'Model: {airplane[1]}')
@@ -125,10 +144,12 @@ def search_and_print_airplane():
         print(f'No airplane found with id{id}')
     db.close()
 
+#function 9
 def print_all_manufacturers():
     '''print all the manufacturers'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    #print all manufacturers information
     sql = 'SELECT * FROM manufacturers'
     cursor.execute(sql)
     results = cursor.fetchall()
